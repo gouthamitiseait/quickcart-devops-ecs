31cbe8e696eedea018ca84f10517bbb8ae725fcb
Gouthami T <151714607+gouthamitiseait@users.noreply.github.com>
2026-06-11 20:12:44 +0530

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -10,7 +10,7 @@ resource "aws_db_subnet_group" "rds_subnet" {
 resource "aws_db_instance" "postgres" {
     identifier              = "${var.project_name}-db"
     engine                  = "postgres"
-    engine_version          = "15.5"
+    engine_version          = "15"
     instance_class          = var.db_instance_class
     allocated_storage       = var.db_allocated_storage
     storage_type            = "gp3"
@@ -18,7 +18,7 @@ resource "aws_db_instance" "postgres" {
     password                = var.db_password
     db_subnet_group_name    = aws_db_subnet_group.rds_subnet.name
     vpc_security_group_ids  = [aws_security_group.RDS_sg.id]
-    publicly_accessible     = false  # Set to true for public access
+    publicly_accessible     = true  # Set to true for public access
     multi_az                = false
     skip_final_snapshot     = true
     deletion_protection     = false
