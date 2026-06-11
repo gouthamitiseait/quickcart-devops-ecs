0bf01239fbcf5024931b5ea4d86fc585d31dbefb
Gouthami T <151714607+gouthamitiseait@users.noreply.github.com>
2026-06-11 18:27:30 +0530

    Update ecs-task-email.tf

--- terraform/ecs-task-email.tf
+++ terraform/ecs-task-email.tf
@@ -5,30 +5,52 @@ resource "aws_ecs_task_definition" "email" {
   cpu                      = "256"
   memory                   = "512"
 
-  execution_role_arn = "arn:aws:iam::775826428475:role/ecsTaskExecutionRole"
-  task_role_arn      = "arn:aws:iam::775826428475:role/ecsTaskExecutionRole"
+  execution_role_arn = "arn:aws:iam::286668306333:role/ecsTaskExecutionRole"
+  task_role_arn      = "arn:aws:iam::286668306333:role/ecsTaskExecutionRole"
 
   container_definitions = jsonencode([
     {
       name       = "email-service"
-      image      = "775826428475.dkr.ecr.ap-south-1.amazonaws.com/email:${var.email_image_tag}"
+      image      = "286668306333.dkr.ecr.ap-south-1.amazonaws.com/email:${var.email_image_tag}"
       essential  = true
+
       portMappings = [
         {
           containerPort = 5006
           hostPort      = 5006
         }
       ]
+
       secrets = [
-        { name = "JWT_SECRET", valueFrom = "arn:aws:secretsmanager:ap-south-1:775826428475:secret:/quickcart/backend/common-rL3fc5:JWT_SECRET::" },
-        { name = "NODE_ENV", valueFrom = "arn:aws:secretsmanager:ap-south-1:775826428475:secret:/quickcart/backend/common-rL3fc5:NODE_ENV::" }
+        {
+          name      = "JWT_SECRET"
+          valueFrom = "arn:aws:secretsmanager:ap-south-1:286668306333:secret:/quickcart/backend/common-rL3fc5:JWT_SECRET::"
+        },
+        {
+          name      = "NODE_ENV"
+          valueFrom = "arn:aws:secretsmanager:ap-south-1:286668306333:secret:/quickcart/backend/common-rL3fc5:NODE_ENV::"
+        }
       ]
+
       environment = [
-        { name = "AWS_REGION", value = "ap-south-1" },
-        { name = "S3_BUCKET_NAME", value = "devops-project-frontend-rajesh" },
-        { name="EMAIL_USER", value="rajesha100920@gmail.com"},
-        { name="EMAIL_PASSWORD",value="ijkl rtny jycm bvjc"}
+        {
+          name  = "AWS_REGION"
+          value = "ap-south-1"
+        },
+        {
+          name  = "S3_BUCKET_NAME"
+          value = "devops-project-frontend-rajesh"
+        },
+        {
+          name  = "EMAIL_USER"
+          value = "rajesha100920@gmail.com"
+        },
+        {
+          name  = "EMAIL_PASSWORD"
+          value = "ijkl rtny jycm bvjc"
+        }
       ]
+
       logConfiguration = {
         logDriver = "awslogs"
         options = {
